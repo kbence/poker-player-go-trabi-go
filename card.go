c7afd0a31d9b4e43d74f778e316ec16976aaddc1
Siôn le Roux <sinisterstuf@gmail.com>
2018-10-06 10:53:03 +0200

    Add Value function for Card

--- card.go
+++ card.go
@@ -1,5 +1,7 @@
 package main
 
+import "strconv"
+
 // Card describes the properties of a card in the game state JSON
 type Card struct {
 	// Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
@@ -7,4 +9,20 @@ type Card struct {
 
 	// Suit of the card. Possible values are: clubs,spades,hearts,diamonds
 	Suit string `json:"suit"`
+}
+
+func (c Card) Value() float64 {
+	switch c.Rank {
+	case "A":
+		return 10.0
+	case "K":
+		return 8.0
+	case "Q":
+		return 7.0
+	case "J":
+		return 6.0
+	default:
+		x, _ := strconv.ParseFloat(c.Rank, 64)
+		return x / 2.0
+	}
 }
