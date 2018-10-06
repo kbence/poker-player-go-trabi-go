e916bf0c79d12df4e56db4a2e122f91184264c24
Bence Kiglics <bence.kiglics@gmail.com>
2018-10-06 14:04:12 +0200

    Go fmtd card.go

--- card.go
+++ card.go
@@ -47,9 +47,9 @@ func (c Card) Position() int {
 	}
 }
 
-func(c Card) gap(other Card)  int {
-	p1, p2:= c.Position(), other.Position()
-	return int(math.Abs(float64(p1-p2)))
+func (c Card) gap(other Card) int {
+	p1, p2 := c.Position(), other.Position()
+	return int(math.Abs(float64(p1 - p2)))
 }
 
 type Hole []Card
@@ -57,24 +57,34 @@ type Hole []Card
 func (h Hole) Value() float64 {
 	a, b := h[0], h[1]
 	value := math.Max(a.Value(), b.Value())
-	if(a.Value()== b.Value()) {value*=2}
+	if a.Value() == b.Value() {
+		value *= 2
+	}
 	inSuit := a.Suit == b.Suit
-	if inSuit {value+=2}
-	g:=a.gap(b)
+	if inSuit {
+		value += 2
+	}
+	g := a.gap(b)
 
 	switch g {
-	case 0:break
-	case 1:value -= 1
-	case 2:value-=2
-	case 3:value-=4
+	case 0:
+		break
+	case 1:
+		value -= 1
+	case 2:
+		value -= 2
+	case 3:
+		value -= 4
 	default:
-		value-=5
+		value -= 5
 	}
 
-	if (a.Position()<12 && b.Position()<12 && g<=1) {value+=1}
+	if a.Position() < 12 && b.Position() < 12 && g <= 1 {
+		value += 1
+	}
 
 	rounded := math.Round(value)
-	log.Printf("%s %s %s %s [v1=%v v2=%v g=%v %v] => %v", a.Rank ,a.Suit, b.Rank, a.Suit,
+	log.Printf("%s %s %s %s [v1=%v v2=%v g=%v %v] => %v", a.Rank, a.Suit, b.Rank, a.Suit,
 		a.Value(), b.Value(), g, inSuit,
 		rounded)
 	return rounded
